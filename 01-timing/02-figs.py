509ccf01fa3b12a7a9668bfe6279f91c4dbd3be9
peter <petrel.harp@gmail.com>
2025-10-30 11:50:35 -0700

    updated lines on runtime plot

--- 01-timing/02-figs.py
+++ 01-timing/02-figs.py
@@ -1,33 +1,101 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker
 import pandas as pd
+import statsmodels.formula.api as smf
 
-import tskit
-import msprime
-import demes
+fname = "runtime_data.csv"
+try:
+    # this is allowing us to adjust the plotting outside of snakemake
+    from snakemake.script import snakemake
+    times = pd.concat([pd.DataFrame(np.load(time_path)) for time_path in snakemake.input.runtimes], axis=0)
+    np.savetxt(fname, times, delimiter=",", comments="",
+               fmt=["%f", "%d", "%d", "%d"],
+               header=",".join(["time", "num_individuals", "num_nodes", "num_edges"]))
+except:
+    print("Not re-generating CSV file!")
+    pass
 
-from snakemake.script import snakemake
+try:
+    outpath = snakemake.output.save_path
+except:
+    outpath = "figs/runtime.pdf"
 
-times = pd.concat([pd.DataFrame(np.load(time_path)) for time_path in snakemake.input.runtimes], axis=0)
+times = pd.read_csv(fname)
+times['num_individuals'] /= 1e3
+times['num_edges'] /= 1e6
+times['num_nodes'] /= 1e6
 
-fig, ax = plt.subplots(1, 3, figsize=(4*3, 4), sharey=True)
+times['log_edges'] = np.log(times['num_edges'])
+times['log_indivs'] = np.log(times['num_individuals'])
+times['log_nodes'] = np.log(times['num_nodes'])
+times['log_time'] = np.log(times['time'])
+
+# the main fit
+fit = smf.ols(formula='time ~ num_individuals + num_edges : np.log(num_individuals)', data=times).fit()
+
+print(fit.summary())
+print(fit.params)
+
+# fits used to make smooth lines for the univariate plots
+edge_fit = smf.ols(formula='log_edges ~ log_indivs + I(log_indivs**2)', data=times).fit()
+indiv_fit = smf.ols(formula='log_indivs ~ log_edges + I(log_edges**2)', data=times).fit()
+node_indiv_fit = smf.ols(formula='log_indivs ~ log_nodes + I(log_nodes**2)', data=times).fit()
+node_edge_fit = smf.ols(formula='log_edges ~ log_nodes + I(log_nodes**2)', data=times).fit()
+
+fig, ax = plt.subplots(1, 3, figsize=(2*3, 2.2), sharey=True)
 
 # individual
-gby_mean = times.groupby(1)[0].mean()
-ax[0].plot(gby_mean.index, gby_mean)
-ax[0].set_xlabel('Number of individuals', fontsize=12)
-ax[0].set_ylabel('Time (s)', fontsize=12)
+gby_mean = times.groupby('num_individuals')['time'].mean()
+pred = pd.DataFrame({
+        'num_individuals' : np.linspace(times['num_individuals'].min(), times['num_individuals'].max(), 101),
+    })
+pred['log_indivs'] = np.log(pred['num_individuals'])
+pred['num_edges'] = np.exp(edge_fit.predict(pred))
+pred['time'] = fit.predict(pred)
 
-gby_mean = times.groupby(2)[0].mean()
-ax[1].plot(gby_mean.index, gby_mean)
-ax[1].set_xlabel('Number of nodes', fontsize=12)
+ax[0].scatter(gby_mean.index, gby_mean, label='runtime', c='blue')
+ax[0].scatter(times['num_individuals'], times['time'], marker='.', c='blue')
+ax[0].plot(pred['num_individuals'], pred['time'], c='red', label='model')
+ax[0].set_xlabel('Thousands of individuals', fontsize=10)
+ax[0].set_ylabel('Time (s)', fontsize=10)
+# ax[0].legend()
 
-gby_mean = times.groupby(3)[0].mean()
-ax[2].plot(gby_mean.index, gby_mean)
-ax[2].set_xlabel('Number of edges', fontsize=12)
+# edge
+gby_mean = times.groupby('num_edges')['time'].mean()
+pred = pd.DataFrame({
+        'num_edges' : np.linspace(times['num_edges'].min(), times['num_edges'].max(), 101),
+    })
+pred['log_edges'] = np.log(pred['num_edges'])
+pred['num_individuals'] = np.exp(indiv_fit.predict(pred))
+pred['time'] = fit.predict(pred)
+
+ax[1].scatter(gby_mean.index, gby_mean, label='runtime', c='blue')
+ax[1].scatter(times['num_edges'], times['time'], marker='.', c='blue')
+ax[1].plot(pred['num_edges'], pred['time'], c='red', label='model')
+ax[1].set_xlabel('Millions of edges', fontsize=10)
+ax[1].set_ylabel('Time (s)', fontsize=10)
+# ax[1].legend()
+
+# node
+gby_mean = times.groupby('num_nodes')['time'].mean()
+pred = pd.DataFrame({
+        'num_nodes' : np.linspace(times['num_nodes'].min(), times['num_nodes'].max(), 101),
+    })
+pred['log_nodes'] = np.log(pred['num_nodes'])
+pred['num_individuals'] = np.exp(node_indiv_fit.predict(pred))
+pred['num_edges'] = np.exp(node_edge_fit.predict(pred))
+pred['time'] = fit.predict(pred)
+
+ax[2].scatter(gby_mean.index, gby_mean, label='runtime', c='blue')
+ax[2].scatter(times['num_nodes'], times['time'], marker='.', c='blue')
+ax[2].plot(pred['num_nodes'], pred['time'], c='red', label='model')
+ax[2].set_xlabel('Millions of nodes', fontsize=10)
+ax[2].set_ylabel('Time (s)', fontsize=10)
+# ax[2].legend()
 
 for i in range(3):
     ax[i].ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 
 plt.tight_layout()
-plt.savefig(snakemake.output.save_path, bbox_inches='tight')
+plt.savefig(outpath, bbox_inches='tight')
