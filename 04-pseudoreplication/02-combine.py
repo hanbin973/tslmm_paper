11e7391e324423f6f52e32c72a9734f9bea944c9
Hanbin Lee <hanbin973@gmail.com>
2025-12-01 08:39:57 -0500

    variance component benchmark

--- 04-pseudoreplication/02-combine.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-from snakemake.script import snakemake
-
-npys = [np.load(path) for path in snakemake.input.npy_paths]
-arr = np.vstack(npys)
-
-np.save(snakemake.output[0], arr)
